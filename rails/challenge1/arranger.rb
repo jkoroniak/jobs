3c86295ce670e629738e4defeb8884e05012a94e
Joel Koroniak <jkoroniak@gmail.com>
2016-08-03 13:37:16 -0400

    Fixed typo in expected output

--- rails/challenge1/arranger.rb
+++ rails/challenge1/arranger.rb
@@ -1,7 +1,7 @@
 class Arranger
   class << self
     # Arrange str such that all the words are arranged in non-descending order
-    # For example, arrange_string("Be the change you want to see in the world") -> "Be in to see the the you want change world"
+    # For example, arrange_string("Be the change you want to see in the world") -> "Be in to see the the you want world change"
     def arrange_string(str)
       
 
